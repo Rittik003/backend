a6b06825e7d30aa43bf27a294c39104a90f5d37d
Rittik003 <90068557+Rittik003@users.noreply.github.com>
2023-12-26 12:20:28 +0530

    some small debugging and adding userControllers and userRoutes

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
- 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
- 
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
 
 
 
-connectDB() 
+connectDB()
 .then(() => {
-    app.on("error",(error)=>{
-        console.log("ERRR:",error);
-        throw error
-    })
-
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️   Server is running at port : ${process.env.PORT}`);
+        console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
+}) 
 
 
 
+// connectDB() 
+// .then(() => {
+//     app.on("error",(error)=>{
+//         console.log("ERRR:",error);
+//         throw error
+//     })
+
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`⚙️   Server is running at port : ${process.env.PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.log("MONGO db connection failed !!! ", err);
+// })
+
+
+//APPROACH 1
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
 // import express from "express"
 // const app=express()
 
